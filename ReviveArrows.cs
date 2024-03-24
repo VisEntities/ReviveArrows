699a347fb6931bdbabbf3891bbffd5f8348ed713
VisEntities <visentities@gmail.com>
2024-03-24 02:47:44 +0100

    Refactored and improved messaging

--- ReviveArrows.cs
+++ ReviveArrows.cs
@@ -1,19 +1,25 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using Network;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Rewritten from scratch and maintained to present by VisEntities
+ * Originally created by redBDGR, up to version 1.0.1
+ */
+
 namespace Oxide.Plugins
 {
-    [Info("Revive Arrows", "Dana", "2.1.0")]
-    [Description("Shoot your teammates with love and healing.")]
-
+    [Info("Revive Arrows", "VisEntities", "3.0.0")]
+    [Description("Heal and revive the wounded from a distance.")]
     public class ReviveArrows : RustPlugin
     {
         #region Fields
 
-        private static ReviveArrows _instance;
+        private static ReviveArrows _plugin;
         private static Configuration _config;
+
+        private const string FX_INJECT_FRIEND = "assets/prefabs/tools/medical syringe/effects/inject_friend.prefab";
 
         #endregion Fields
 
@@ -24,41 +30,70 @@ namespace Oxide.Plugins
             [JsonProperty("Version")]
             public string Version { get; set; }
 
-            [JsonProperty("Revive Arrow Button")]
-            public string ReviveArrowButton { get; set; }
+            [JsonProperty("Instant Health Increase")]
+            public float InstantHealthIncrease { get; set; }
 
-            [JsonProperty("Instant Heal Amount")]
-            public float InstantHealAmount { get; set; }
+            [JsonProperty("Health Increase Over Time")]
+            public float HealthIncreaseOverTime { get; set; }
 
-            [JsonProperty("Heal Amount Over Time")]
-            public float HealAmountOverTime { get; set; }
+            [JsonProperty("Can Revive Wounded")]
+            public bool CanReviveWounded { get; set; }
+            
+            [JsonProperty("Arrow Ingredients")]
+            public List<ItemInfo> ArrowIngredients { get; set; }
+        }
 
-            [JsonProperty("Can Revive")]
-            public bool CanRevive { get; set; }
+        public class ItemInfo
+        {
+            [JsonProperty("Shortname")]
+            public string Shortname { get; set; }
 
-            [JsonProperty("Revive Sound Effect")]
-            public string ReviveSoundEffect { get; set; }
-
-            [JsonProperty("Consumable Items")]
-            public List<ConsumableOptions> ConsumableItems { get; set; }
+            [JsonProperty("Amount")]
+            public int Amount { get; set; }
 
             [JsonIgnore]
-            public BUTTON Button
+            private bool _validated;
+
+            [JsonIgnore]
+            private ItemDefinition _itemDefinition;
+
+            [JsonIgnore]
+            public ItemDefinition ItemDefinition
             {
                 get
                 {
-                    return (BUTTON)Enum.Parse(typeof(BUTTON), ReviveArrowButton);
+                    if (!_validated)
+                    {
+                        ItemDefinition matchedItemDefinition = ItemManager.FindItemDefinition(Shortname);
+                        if (matchedItemDefinition != null)
+                            _itemDefinition = matchedItemDefinition;
+                        else
+                            return null;
+
+                        _validated = true;
+                    }
+
+                    return _itemDefinition;
                 }
             }
-        }
 
-        private class ConsumableOptions
-        {
-            [JsonProperty("Item Shortname")]
-            public string ItemShortname { get; set; }
+            public int GetItemAmount(ItemContainer container)
+            {
+                return container.GetAmount(ItemDefinition.itemid, true);
+            }
 
-            [JsonProperty("Amount To Consume")]
-            public int AmountToConsume { get; set; }
+            public void GiveItem(BasePlayer player, ItemContainer container)
+            {
+                container.GiveItem(ItemManager.CreateByItemID(ItemDefinition.itemid, Amount));
+                player.Command("note.inv", ItemDefinition.itemid, Amount);
+            }
+
+            public int TakeItem(BasePlayer player, ItemContainer container)
+            {
+                int amountTaken = container.Take(null, ItemDefinition.itemid, Amount);
+                player.Command("note.inv", ItemDefinition.itemid, -amountTaken);
+                return amountTaken;
+            }
         }
 
         protected override void LoadConfig()
@@ -82,48 +117,41 @@ namespace Oxide.Plugins
             Config.WriteObject(_config, true);
         }
 
-        private Configuration GetDefaultConfig()
-        {
-            return new Configuration
-            {
-                Version = Version.ToString(),
-                ReviveArrowButton = "USE",
-                InstantHealAmount = 15f,
-                HealAmountOverTime = 20f,
-                CanRevive = true,
-                ReviveSoundEffect = "assets/prefabs/tools/medical syringe/effects/inject_friend.prefab",
-                ConsumableItems = new List<ConsumableOptions>()
-                    {
-                        new ConsumableOptions
-                        {
-                            ItemShortname = "syringe.medical",
-                            AmountToConsume = 1,
-                        },
-                        new ConsumableOptions
-                        {
-                            ItemShortname = "rope",
-                            AmountToConsume = 1,
-                        },
-                    }
-            };
-        }
-
         private void UpdateConfig()
         {
-            PrintWarning("Detected changes in configuration! Updating...");
+            PrintWarning("Config changes detected! Updating...");
+
             Configuration defaultConfig = GetDefaultConfig();
 
             if (string.Compare(_config.Version, "1.0.0") < 0)
                 _config = defaultConfig;
 
-            if (string.Compare(_config.Version, "2.1.0") < 0)
-            {
-                _config.ReviveArrowButton = defaultConfig.ReviveArrowButton;
-                _config.ReviveSoundEffect = defaultConfig.ReviveSoundEffect;
-            }
-
-            PrintWarning("Configuration update complete! Updated from version " + _config.Version + " to " + Version.ToString());
+            PrintWarning("Config update complete! Updated from version " + _config.Version + " to " + Version.ToString());
             _config.Version = Version.ToString();
+        }
+
+        private Configuration GetDefaultConfig()
+        {
+            return new Configuration
+            {
+                Version = Version.ToString(),
+                InstantHealthIncrease = 15f,
+                HealthIncreaseOverTime = 20f,
+                CanReviveWounded = true,
+                ArrowIngredients = new List<ItemInfo>
+                {
+                    new ItemInfo
+                    {
+                        Shortname = "syringe.medical",
+                        Amount = 1
+                    },
+                    new ItemInfo
+                    {
+                        Shortname = "rope",
+                        Amount = 1
+                    }
+                }
+            };
         }
 
         #endregion Configuration
@@ -132,121 +160,119 @@ namespace Oxide.Plugins
 
         private void Init()
         {
-            _instance = this;
-            PermissionUtils.Register();
+            _plugin = this;
+            PermissionUtil.RegisterPermissions();
         }
 
         private void Unload()
         {
             _config = null;
-            _instance = null;
+            _plugin = null;
         }
 
-        private void OnPlayerAttack(BasePlayer player, HitInfo hitInfo)
+        private void OnActiveItemChanged(BasePlayer player, Item oldItem, Item newItem)
         {
-            if (!player.IsValid() || !hitInfo.HitEntity.IsValid())
+            if (player == null || !PermissionUtil.VerifyHasPermission(player))
                 return;
 
-            if (!CanUseHealArrow(player, hitInfo))
+            if (!newItem.info.shortname.Contains("bow"))
                 return;
 
-            BasePlayer targetPlayer = hitInfo.HitEntity.ToPlayer();
-            if (!targetPlayer.IsValid())
-                return;
+            SendGameTip(player, lang.GetMessage(Lang.HealArrowUsage, this, player.UserIDString), 5f);
+        }
 
-            Dictionary<Item, int> itemsToConsume;
-            if (!VerifyHasConsumableItem(player, out itemsToConsume))
+        private object OnPlayerAttack(BasePlayer player, HitInfo hitInfo)
+        {
+            if (player == null || hitInfo == null)
+                return null;
+
+            if (!PermissionUtil.VerifyHasPermission(player))
+                return null;
+
+            BasePlayer hitPlayer = hitInfo.HitEntity.ToPlayer();
+            if (hitPlayer == null || hitPlayer.IsNpc)
+                return null;
+
+            if (!hitInfo.Weapon.ShortPrefabName.Contains("bow"))
+                return null;
+
+            if (!player.serverInput.IsDown(BUTTON.USE))
+                return null;
+
+            foreach (ItemInfo ingredient in _config.ArrowIngredients)
             {
-                ReplyToPlayer(player, Lang.ReviveInsufficientConsumable);
-                return;
+                int amount = ingredient.GetItemAmount(player.inventory.containerMain);
+                if (amount < ingredient.Amount)
+                {
+                    SendReplyToPlayer(player, Lang.InsufficientIngredients, ingredient.Shortname, ingredient.Amount);
+                    return null;
+                }
             }
 
-            ConsumeItems(player, itemsToConsume);
+            foreach (ItemInfo ingredient in _config.ArrowIngredients)
+            {
+                ingredient.TakeItem(player, player.inventory.containerMain);
+            }
 
-            hitInfo.damageTypes.ScaleAll(0);
-            HealPlayer(targetPlayer);
-            ReplyToPlayer(player, Lang.ReviveSuccess);
+            Heal(hitPlayer);
+            RunEffect(FX_INJECT_FRIEND, hitPlayer, boneId: 698017942);
+            SendGameTip(player, lang.GetMessage(Lang.PlayerHealed, this, player.UserIDString), 5f, hitPlayer.displayName, _config.InstantHealthIncrease);
+
+            return true;
         }
 
         #endregion Oxide Hooks
 
         #region Functions
 
-        private bool CanUseHealArrow(BasePlayer player, HitInfo hitInfo)
+        private void Heal(BasePlayer player)
         {
-            if (!player.serverInput.IsDown(_config.Button))
-                return false;
-
-            if (!PermissionUtils.Verify(player))
-                return false;
-
-            if (!hitInfo.Weapon.ShortPrefabName.ToLower().Contains("bow"))
-                return false;
-
-            return true;
-        }
-
-        private bool VerifyHasConsumableItem(BasePlayer player, out Dictionary<Item, int> items)
-        {
-            items = new Dictionary<Item, int>();
-            foreach (ConsumableOptions consumable in _config.ConsumableItems)
+            player.Heal(_config.InstantHealthIncrease);
+            player.metabolism.ApplyChange(MetabolismAttribute.Type.HealthOverTime, _config.HealthIncreaseOverTime, 1f);
+            
+            if (_config.CanReviveWounded)
             {
-                Item item = player.inventory.FindItemID(consumable.ItemShortname);
-                if (item == null || item.amount < consumable.AmountToConsume)
-                    return false;
-                else
-                    items.Add(item, consumable.AmountToConsume);
-            }
-
-            return true;
-        }
-
-        private void ConsumeItems(BasePlayer player, Dictionary<Item, int> items)
-        {
-            foreach (var item in items)
-                player.inventory.Take(null, item.Key.info.itemid, item.Value);
-        }
-
-        private void HealPlayer(BasePlayer player)
-        {
-            player.Heal(_config.InstantHealAmount);
-            player.metabolism.ApplyChange(MetabolismAttribute.Type.HealthOverTime, _config.HealAmountOverTime, 1f);
-
-            if (_config.CanRevive && player.IsWounded())
                 player.StopWounded();
-
-            PlayEffect(_config.ReviveSoundEffect, player);
-            ReplyToPlayer(player, Lang.ReviveSuccess);
-        }
-
-        private void PlayEffect(string effectPrefab, BasePlayer player)
-        {
-            if (string.IsNullOrEmpty(effectPrefab))
-                return;
-
-            Effect.server.Run(effectPrefab, player, 827230707, Vector3.up, new Vector3(), player.net.connection, false);
+            }
         }
 
         #endregion Functions
 
+        #region Helper Functions
+
+        private void SendGameTip(BasePlayer player, string message, float durationSeconds, params object[] args)
+        {
+            message = string.Format(message, args);
+
+            player.SendConsoleCommand("gametip.showgametip", message);
+            timer.Once(durationSeconds, () =>
+            {
+                if (player != null)
+                    player.SendConsoleCommand("gametip.hidegametip");
+            });
+        }
+
+        private static void RunEffect(string prefab, BaseEntity entity, uint boneId = 0, Vector3 localPosition = default(Vector3), Vector3 localDirection = default(Vector3), Connection effectRecipient = null, bool sendToAll = false)
+        {
+            Effect.server.Run(prefab, entity, boneId, localPosition, localDirection, effectRecipient, sendToAll);
+        }
+
+        #endregion Helper Functions
+
         #region Helper Classes
 
-        private static class PermissionUtils
+        private static class PermissionUtil
         {
             public const string USE = "revivearrows.use";
 
-            public static void Register()
+            public static void RegisterPermissions()
             {
-                _instance.permission.RegisterPermission(USE, _instance);
+                _plugin.permission.RegisterPermission(USE, _plugin);
             }
 
-            public static bool Verify(BasePlayer player, string permissionName = USE)
+            public static bool VerifyHasPermission(BasePlayer player, string permissionName = USE)
             {
-                if (_instance.permission.UserHasPermission(player.UserIDString, permissionName))
-                    return true;
-
-                _instance.ReplyToPlayer(player, Lang.NoPermission);
-                return false;
+                return _plugin.permission.UserHasPermission(player.UserIDString, permissionName);
             }
         }
 
@@ -256,30 +282,29 @@ namespace Oxide.Plugins
 
         private class Lang
         {
-            public const string NoPermission = "NoPermission";
-            public const string ReviveSuccess = "Revive.Success";
-            public const string ReviveInsufficientConsumable = "Revive.InsufficientConsumable";
+            public const string InsufficientIngredients = "InsufficientIngredients";
+            public const string PlayerHealed = "PlayerHealed ";
+            public const string HealArrowUsage = "HealArrowUsage ";
         }
 
         protected override void LoadDefaultMessages()
         {
             lang.RegisterMessages(new Dictionary<string, string>
             {
-                [Lang.NoPermission] = "You lack the necessary permission to use heal arrows.",
-                [Lang.ReviveSuccess] = "Your arrow has done its job, the target has been healed!",
-                [Lang.ReviveInsufficientConsumable] = "Sorry, you cannot use revive revive without the required materials!",
+                [Lang.InsufficientIngredients] = "You don't have enough <color=#FABE28>{0}</color>. Required: <color=#FABE28>{1}</color>",
+                [Lang.PlayerHealed] = "You healed <color=#FABE28>{0}</color> by <color=#FABE28>{1}</color> health points",
+                [Lang.HealArrowUsage] = "Hold down <color=#FABE28>use</color> to heal a friend with an arrow",
 
             }, this, "en");
         }
 
-        private void ReplyToPlayer(BasePlayer player, string messageName, params object[] args)
+        private void SendReplyToPlayer(BasePlayer player, string messageKey, params object[] args)
         {
-            SendReply(player, string.Format(GetLang(messageName, player.UserIDString), args));
-        }
+            string message = lang.GetMessage(messageKey, this, player.UserIDString);
+            if (args.Length > 0)
+                message = string.Format(message, args);
 
-        private string GetLang(string messageName, string playerId = null)
-        {
-            return lang.GetMessage(messageName, this, playerId);
+            SendReply(player, message);
         }
 
         #endregion Localization
